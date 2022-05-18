7d0fefffca45d691489d33b238b61bb6f34bad5f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-05-18 19:09:17 +0530

    Handle exception logic part - 2

--- src/main/java/lk/ijse/dep8/tasks/util/HttpServlet2.java
+++ src/main/java/lk/ijse/dep8/tasks/util/HttpServlet2.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,10 +26,14 @@ public class HttpServlet2 extends HttpServlet {
             logger.logp(Level.SEVERE, t.getStackTrace()[0].getClassName(),
                     t.getStackTrace()[0].getMethodName(), t.getMessage(), t);
 
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+
             resp.setContentType("application/json");
             HttpResponseErrorMsg errorMsg = new HttpResponseErrorMsg(new Date().getTime(),
                     500, "Internal Server Error",
-                    t.getLocalizedMessage(), t.getMessage(), req.getRequestURI());
+                    sw.toString(), t.getMessage(), req.getRequestURI());
             Jsonb jsonb = JsonbBuilder.create();
             jsonb.toJson(errorMsg, resp.getWriter());
         }
