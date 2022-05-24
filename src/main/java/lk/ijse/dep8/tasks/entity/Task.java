ed3ba51e0287fe78f4ff92d16f4d7cae5feaf9b3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-05-24 20:56:09 +0530

    Add Lombok to the project

--- src/main/java/lk/ijse/dep8/tasks/entity/Task.java
+++ src/main/java/lk/ijse/dep8/tasks/entity/Task.java
@@ -1,84 +1,21 @@
 package lk.ijse.dep8.tasks.entity;
 
-public class Task {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Task implements Serializable {
     private int id;
     private String title;
     private String details;
     private int position;
     private Status status;
     private int taskListId;
-
-    public Task() {
-    }
-
-    public Task(int id, String title, String details, int position, Status status, int taskListId) {
-        this.id = id;
-        this.title = title;
-        this.details = details;
-        this.position = position;
-        this.status = status;
-        this.taskListId = taskListId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public int getTaskListId() {
-        return taskListId;
-    }
-
-    public void setTaskListId(int taskListId) {
-        this.taskListId = taskListId;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", details='" + details + '\'' +
-                ", position=" + position +
-                ", status=" + status +
-                ", taskListId=" + taskListId +
-                '}';
-    }
 
     public enum Status{
         completed, needsAction
