765c876916fcb67efd6f5a396cfdb931e16d7ffa
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-05-25 19:01:55 +0530

    Add DAOFactory

--- src/main/java/lk/ijse/dep8/tasks/dao/DAOFactory.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/DAOFactory.java
@@ -19,18 +19,22 @@ public class DAOFactory {
         return (daoFactory == null)? (daoFactory = new DAOFactory()): daoFactory;
     }
 
-    public UserDAO getUserDAO(Connection connection){
-        return new UserDAOImpl(connection);
+    public <T extends SuperDAO> T getDAO(Connection connection, DAOTypes daoType){
+        switch (daoType){
+            case USER:
+                return (T) new UserDAOImpl(connection);
+            case TASK_LIST:
+                return (T) new TaskListDAOImpl(connection);
+            case TASK:
+                return (T) new TaskDAOImpl(connection);
+            case QUERY:
+                return (T) new QueryDAOImpl(connection);
+            default:
+                return null;
+        }
     }
 
-    public TaskListDAO getTaskListDAO(Connection connection){
-        return new TaskListDAOImpl(connection);
-    }
-    public TaskDAO getTaskDAO(Connection connection){
-        return new TaskDAOImpl(connection);
-    }
-
-    public QueryDAO getQueryDAO(Connection connection){
-        return new QueryDAOImpl(connection);
+    public enum DAOTypes{
+        USER, TASK_LIST, TASK, QUERY
     }
 }
