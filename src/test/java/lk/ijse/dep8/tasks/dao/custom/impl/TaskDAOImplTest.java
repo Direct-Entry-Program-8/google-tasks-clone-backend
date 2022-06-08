60e6a9d930c25e0b2f18afad51c7739968deff58
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-08 18:38:14 +0530

    Test TaskDAOImpl 2

--- src/test/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskDAOImplTest.java
+++ src/test/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskDAOImplTest.java
@@ -5,24 +5,27 @@ import lk.ijse.dep8.tasks.entity.TaskList;
 import lk.ijse.dep8.tasks.entity.User;
 import lk.ijse.dep8.tasks.service.util.HibernateUtil;
 import org.hibernate.Session;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class TaskDAOImplTest {
 
     private Session session;
-    private TaskDAOImpl taskDAO;
+    private  TaskDAOImpl taskDAO;
+    private int taskListId;
 
     @BeforeEach
     void setUp() {
         session = HibernateUtil.getSessionFactory().openSession();
         taskDAO = new TaskDAOImpl(session);
         session.beginTransaction();
-        session.save(new User("U001", "dulanga@ijse.lk", "admin", "Dulanga", null));
-        session.save(new TaskList("To-do", session.load(User.class, "U001")));
+
+        if (!session.contains(session.get(User.class, "U001"))){
+            session.save(new User("U001", "dulanga@ijse.lk", "admin", "Dulanga", null));
+            taskListId = (int) session.save(new TaskList("To-do", session.load(User.class, "U001")));
+        }
     }
 
     @AfterEach
@@ -40,7 +43,7 @@ class TaskDAOImplTest {
         // given
         Task givenTask = new Task("Complete the DAO Layer",
                 "Something something",
-                1, Task.Status.needsAction, session.load(TaskList.class, 1));
+                1, Task.Status.needsAction, session.get(TaskList.class, taskListId));
         // when
         Task actualTask = taskDAO.save(givenTask);
         // then
