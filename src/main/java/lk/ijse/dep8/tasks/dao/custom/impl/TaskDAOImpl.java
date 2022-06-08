e735815315461b6ca889908ef77297c1456c252c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-08 18:06:08 +0530

    Revamp TaskDAOImpl

--- src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskDAOImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskDAOImpl.java
@@ -1,134 +1,49 @@
 package lk.ijse.dep8.tasks.dao.custom.impl;
 
 import lk.ijse.dep8.tasks.dao.custom.TaskDAO;
-import lk.ijse.dep8.tasks.dao.exception.DataAccessException;
 import lk.ijse.dep8.tasks.entity.Task;
+import org.hibernate.Session;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class TaskDAOImpl implements TaskDAO {
 
-    private final Connection connection;
+    private Session session;
 
-    public TaskDAOImpl(Connection connection) {
-        this.connection = connection;
+    public TaskDAOImpl(Session session) {
+        this.session = session;
     }
 
     @Override
-    public boolean existsById(Integer taskId) {
-        try {
-            PreparedStatement stm = connection.prepareStatement("SELECT id FROM task WHERE id=?");
-            stm.setInt(1, taskId);
-            return stm.executeQuery().next();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public boolean existsById(Integer pk) {
+        return findById(pk).isPresent();
     }
 
     @Override
-    public Task save(Task task) {
-        try {
-            if (!existsById(task.getId())) {
-                PreparedStatement stm = connection.
-                        prepareStatement("INSERT INTO task (title, details, position, status, task_list_id) VALUES (?, ?, ?, ?, ?)");
-                stm.setString(1, task.getTitle());
-                stm.setString(2, task.getDetails());
-                stm.setInt(3, task.getPosition());
-                stm.setString(4, task.getStatus().toString());
-                stm.setInt(5, task.getTaskListId());
-                if (stm.executeUpdate() != 1) {
-                    throw new SQLException("Failed to save the task");
-                }
-            } else {
-                PreparedStatement stm = connection.
-                        prepareStatement("UPDATE task SET title=?, details=?, position=?, status=?, task_list_id=? WHERE id=?");
-                stm.setString(1, task.getTitle());
-                stm.setString(2, task.getDetails());
-                stm.setInt(3, task.getPosition());
-                stm.setString(4, task.getStatus().toString());
-                stm.setInt(5, task.getTaskListId());
-                stm.setInt(6, task.getId());
-                if (stm.executeUpdate() != 1) {
-                    throw new SQLException("Failed to update the task");
-                }
-            }
-            return task;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public Task save(Task entity) {
+        session.save(entity);
+        return entity;
     }
 
     @Override
-    public void deleteById(Integer taskId) {
-        try {
-            if (!existsById(taskId)){
-                throw new DataAccessException("No task found");
-            }
-            PreparedStatement stm = connection.prepareStatement("DELETE FROM task WHERE id=?");
-            stm.setInt(1, taskId);
-            if (stm.executeUpdate() != 1) {
-                throw new SQLException("Failed to delete the task");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void deleteById(Integer pk) {
+        session.delete(session.load(Task.class, pk));
     }
 
     @Override
-    public Optional<Task> findById(Integer taskId) {
-        try {
-            PreparedStatement stm = connection.prepareStatement("SELECT * FROM task WHERE id=?");
-            stm.setInt(1, taskId);
-            ResultSet rst = stm.executeQuery();
-            if (rst.next()){
-                return Optional.of(new Task(rst.getInt("id"),
-                        rst.getString("title"),
-                        rst.getString("details"),
-                        rst.getInt("position"),
-                        Task.Status.valueOf(rst.getString("status")),
-                        rst.getInt("task_list_id")));
-            }else{
-                return Optional.empty();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public Optional<Task> findById(Integer pk) {
+        Task task = session.get(Task.class, pk);
+        return task == null ? Optional.empty(): Optional.of(task);
     }
 
     @Override
     public List<Task> findAll() {
-        try {
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("SELECT * FROM task");
-            List<Task> tasks = new ArrayList<>();
-            while (rst.next()) {
-                tasks.add(new Task(rst.getInt("id"),
-                        rst.getString("title"),
-                        rst.getString("details"),
-                        rst.getInt("position"),
-                        Task.Status.valueOf(rst.getString("status")),
-                        rst.getInt("task_list_id")));
-            }
-            return tasks;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return session.createQuery("FROM Task t", Task.class).list();
     }
 
     @Override
     public long count() {
-        try {
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("SELECT COUNT(id) AS count FROM user");
-            if (rst.next()){
-                return rst.getLong("count");
-            }
-            return 0;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return session.createQuery("SELECT COUNT(t) FROM Task t", Long.class).uniqueResult();
     }
 }
