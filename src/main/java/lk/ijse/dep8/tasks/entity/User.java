c20266e4afce81246dc426b95969d65de43500ff
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-08 14:52:37 +0530

    Revamp UserDAOImpl2 to support Hibernate

--- src/main/java/lk/ijse/dep8/tasks/entity/User.java
+++ src/main/java/lk/ijse/dep8/tasks/entity/User.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class User implements SuperEntity {
+    @Id
     private String id;
     private String email;
     private String password;
