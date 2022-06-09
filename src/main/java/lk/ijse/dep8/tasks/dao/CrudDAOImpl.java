ce6559c5a9c0ecb27b16d2da8846a95c4a67cb4c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-09 13:00:28 +0530

    Finalize CrudDAOImpl

--- src/main/java/lk/ijse/dep8/tasks/dao/CrudDAOImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/CrudDAOImpl.java
@@ -1,17 +1,22 @@
 package lk.ijse.dep8.tasks.dao;
 
 import lk.ijse.dep8.tasks.entity.SuperEntity;
-import lk.ijse.dep8.tasks.entity.User;
 import org.hibernate.Session;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class CrudDAOImpl<T extends SuperEntity, ID extends Serializable>
         implements CrudDAO<T, ID> {
 
+    private final Class<T> entityClsObj;
     protected Session session;
+
+    public CrudDAOImpl() {
+        entityClsObj = (Class<T>) (((ParameterizedType) (this.getClass().getGenericSuperclass())).getActualTypeArguments()[0]);
+    }
 
     @Override
     public boolean existsById(ID pk) {
@@ -26,22 +31,22 @@ public abstract class CrudDAOImpl<T extends SuperEntity, ID extends Serializable
 
     @Override
     public void deleteById(ID pk) {
-        //session.delete(session.load(T, pk));
+        session.delete(session.load(entityClsObj, pk));
     }
 
     @Override
     public Optional<T> findById(ID pk) {
-//        T entity = session.get(T, pk);
-//        return (entity == null) ? Optional.empty() : Optional.of(entity);
+        T entity = session.get(entityClsObj, pk);
+        return (entity == null) ? Optional.empty() : Optional.of(entity);
     }
 
     @Override
     public List<T> findAll() {
-//        return session.createQuery("FROM T", T.class).list();
+        return session.createQuery("FROM " + entityClsObj.getName(), entityClsObj).list();
     }
 
     @Override
     public long count() {
-//        return session.createQuery("SELECT COUNT(entity) FROM T entity", Long.class).uniqueResult();
+        return session.createQuery("SELECT COUNT(entity) FROM "+ entityClsObj.getName() +" entity", Long.class).uniqueResult();
     }
 }
--- src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/UserDAOImpl.java
@@ -14,38 +14,6 @@ public class UserDAOImpl extends CrudDAOImpl<User, String> implements UserDAO {
         this.session = session;
     }
 
-//    @Override
-//    public boolean existsById(String pk) {
-//        return findById(pk).isPresent();
-//    }
-//
-//    @Override
-//    public User save(User entity) {
-//        session.save(entity);
-//        return entity;
-//    }
-//
-//    @Override
-//    public void deleteById(String pk) {
-//        session.delete(session.load(User.class, pk));
-//    }
-//
-//    @Override
-//    public Optional<User> findById(String pk) {
-//        User user = session.get(User.class, pk);
-//        return (user == null) ? Optional.empty() : Optional.of(user);
-//    }
-//
-//    @Override
-//    public List<User> findAll() {
-//        return session.createQuery("FROM User u", User.class).list();
-//    }
-//
-//    @Override
-//    public long count() {
-//        return session.createQuery("SELECT COUNT(u) FROM User u", Long.class).uniqueResult();
-//    }
-
     @Override
     public boolean existsUserByEmailOrId(String emailOrId) {
         return findUserByIdOrEmail(emailOrId).isPresent();
