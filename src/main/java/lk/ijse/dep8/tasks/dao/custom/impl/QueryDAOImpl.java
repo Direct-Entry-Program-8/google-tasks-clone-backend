d629993e132aa897ad4f48514c1669be623c797f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-09 11:44:44 +0530

    Revamp QueryDAOImpl

--- src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/QueryDAOImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/QueryDAOImpl.java
@@ -1,15 +1,16 @@
 package lk.ijse.dep8.tasks.dao.custom.impl;
 
 import lk.ijse.dep8.tasks.dao.custom.QueryDAO;
+import org.hibernate.Session;
 
 import java.sql.Connection;
 
 public class QueryDAOImpl implements QueryDAO {
 
-    private final Connection connection;
+    private final Session session;
 
-    public QueryDAOImpl(Connection connection) {
-        this.connection = connection;
+    public QueryDAOImpl(Session session) {
+        this.session = session;
     }
 
 }
