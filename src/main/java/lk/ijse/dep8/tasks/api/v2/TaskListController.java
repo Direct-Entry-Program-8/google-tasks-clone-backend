47b5ee0e33990ce55e3c42ad6a0451cb5cfbac73
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-22 19:40:06 +0530

    Revamp saveUser() api

---
+++ src/main/java/lk/ijse/dep8/tasks/api/v2/TaskListController.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep8.tasks.api.v2;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskListController {
+}
---
+++ src/main/java/lk/ijse/dep8/tasks/api/v2/UserController.java
@@ -0,0 +1,67 @@
+package lk.ijse.dep8.tasks.api.v2;
+
+import lk.ijse.dep8.tasks.dto.UserDTO;
+import lk.ijse.dep8.tasks.service.custom.UserService;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@RestController
+@RequestMapping("v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public UserDTO saveUser(String name, String email, String password, Part picture,
+                         HttpServletRequest request) {
+        if (name == null || !name.matches("[A-Za-z ]+")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid name or name is empty");
+        } else if (email == null || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid email or email is empty");
+        } else if (password == null || password.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Password can't be empty");
+        } else if (picture != null && (picture.getSize() == 0 || !picture.getContentType().startsWith("image"))) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid picture");
+        }
+
+        if (userService.existsUser(email)) {
+            throw new ResponseStatusException(HttpServletResponse.SC_CONFLICT, "A user has been already registered with this email");
+        }
+
+        String pictureUrl = null;
+        if (picture != null) {
+            pictureUrl = request.getScheme() + "://" + request.getServerName() + ":"
+                    + request.getServerPort() + request.getContextPath() + "/uploads/";
+        }
+        UserDTO user = new UserDTO(null, name, email, password, pictureUrl);
+
+        return userService.registerUser(picture,
+                request.getServletContext().getRealPath("/"), user);
+    }
+
+    @DeleteMapping
+    public void deleteUser() {
+
+    }
+
+    @PatchMapping
+    public void updateUser() {
+
+    }
+
+    @GetMapping
+    public void getUser() {
+
+    }
+}
